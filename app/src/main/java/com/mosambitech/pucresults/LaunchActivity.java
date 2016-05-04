a8f1e151ec1afb2fb9e29d4967ac076f99a34e0a
sandeep.kamath <sandeep.kamath@robosoftin.com>
2016-05-04 17:39:45 +0530

    * Integrated pushbots.

--- app/src/main/java/com/mosambitech/pucresults/LaunchActivity.java
+++ app/src/main/java/com/mosambitech/pucresults/LaunchActivity.java
@@ -2,20 +2,18 @@ package com.mosambitech.pucresults;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 import com.beardedhen.androidbootstrap.TypefaceProvider;
+import com.pushbots.push.Pushbots;
 
 public class LaunchActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -25,11 +23,11 @@ public class LaunchActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch);
+        Pushbots.sharedInstance().init(this);
         TypefaceProvider.registerDefaultIconSets();
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
 
         sslc = (View) findViewById(R.id.btn_sslc);
         cet = (View) findViewById(R.id.btn_cet);
